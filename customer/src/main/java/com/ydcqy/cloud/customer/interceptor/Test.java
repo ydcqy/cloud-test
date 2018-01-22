27731886196460873fb9237067e9c437b46ce3fe
xiaoyu <742108726@qq.com>
2018-01-23 01:41:07 +0800

    #

---
+++ customer/src/main/java/com/ydcqy/cloud/customer/interceptor/Test.java
@@ -0,0 +1,27 @@
+//package com.ydcqy.cloud.customer.interceptor;
+//
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+///**
+// * Created by lenovo on 2018/1/22.
+// */
+//public class Test implements HandlerInterceptor {
+//    @Override
+//    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+//        return false;
+//    }
+//
+//    @Override
+//    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+//
+//    }
+//
+//    @Override
+//    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+//
+//    }
+//}
