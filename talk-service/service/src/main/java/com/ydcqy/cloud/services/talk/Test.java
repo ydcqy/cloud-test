0bdedbfb80c6a9b9d7d9ac7d7c9bf351ebbef202
xiaoyu <742108726@qq.com>
2018-05-09 18:02:48 +0800

    #

--- talk-service/service/src/main/java/com/ydcqy/cloud/services/talk/Test.java
+++ talk-service/service/src/main/java/com/ydcqy/cloud/services/talk/Test.java
@@ -10,15 +10,36 @@ import org.springframework.util.ClassUtils;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * @author xiaoyu
  */
 @Slf4j
 public class Test {
-    public static void main(String[] args) throws IOException {
-        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
-        System.out.println(Arrays.asList(resourceResolver.getResources("mapper/*")).get(0).getURL());
+    public static void main(String[] args) throws  Exception {
+//        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+//        System.out.println(Arrays.asList(resourceResolver.getResources("mapper/*")).get(0).getURL());
+Object obj=new Object();
+new Thread(){
+    @Override
+    public void run() {
+
+        System.out.println(1111);
+        try {
+            synchronized (obj) {
+
+                Thread.sleep(2000);
+                System.out.println(1111);
+            }
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+    }
+}.start();
     }
 public void abc(boolean b){
 
