4051ffaa53507b9b71b8f460804a2a19e6d5440f
xiaoyu <742108726@qq.com>
2018-06-06 18:01:15 +0800

    #

--- grpc-service/src/main/java/com/ydcqy/grpc/GrpcServiceMain.java
+++ grpc-service/src/main/java/com/ydcqy/grpc/GrpcServiceMain.java
@@ -1,9 +1,14 @@
 package com.ydcqy.grpc;
 
+import com.ydcqy.grpc.util.SpringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+
+import java.util.Queue;
 
 /**
  * @author xiaoyu
@@ -16,6 +21,7 @@ public class GrpcServiceMain {
     public static void main(String[] args) {
         SpringApplication.run(GrpcServiceMain.class, args);
         log.info("==========Spring Boot grpc.service启动成功！==========");
-
+        ConfigurableWebApplicationContext bean = SpringUtils.getBean(ConfigurableWebApplicationContext.class);
+        System.out.println(bean);
     }
 }
