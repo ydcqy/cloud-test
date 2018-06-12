5b662a216ac9ceb49da61a97fc59ad23927738e8
xiaoyu <742108726@qq.com>
2018-06-12 18:03:21 +0800

    #

--- scriptfile/centos/other.sh
+++ scriptfile/centos/other.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 #防火墙端口
 -A INPUT -p tcp -m state --state NEW -m tcp --dport 1:65535 -j ACCEPT
-
+#make erLang
+yum install -y ncurses-devel openssl openssl-devel unixODBC-devel gcc-c++
 
