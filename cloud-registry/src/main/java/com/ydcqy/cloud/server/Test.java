4d8a0667bd36db3e7915b601978aa3ae139bceff
xiaoyu <742108726@qq.com>
2018-06-20 18:05:51 +0800

    #

--- cloud-registry/src/main/java/com/ydcqy/cloud/server/Test.java
+++ cloud-registry/src/main/java/com/ydcqy/cloud/server/Test.java
@@ -2,6 +2,10 @@ package com.ydcqy.cloud.server;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
 /**
  * @author xiaoyu
  */
@@ -11,5 +15,17 @@ public class Test {
         int[] i = {1, 2, 3};
         System.out.println(i);
         log.info("123");
+        ByteBuffer byteBuffer = ByteBuffer.wrap("哈".getBytes());
+//        byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        System.out.println(byteBuffer.order());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+        System.out.println(Arrays.toString("哈".getBytes()));
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.println(Runtime.getRuntime().totalMemory()/1024/1024);
+        System.out.println(Runtime.getRuntime().maxMemory()/1024/1024);
+        System.out.println(Runtime.getRuntime().freeMemory()/1024/1024);
+
     }
 }
