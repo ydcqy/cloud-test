196a53934f3f77c4d381388caa11ae3daad1a65a
xiaoyu <742108726@qq.com>
2018-08-19 20:02:14 +0800

    #

--- ycache/ycache-support/src/main/java/com/ydcqy/ycache/zk/ZkClient.java
+++ ycache/ycache-support/src/main/java/com/ydcqy/ycache/zk/ZkClient.java
@@ -1,6 +1,7 @@
 package com.ydcqy.ycache.zk;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
@@ -14,22 +15,36 @@ import java.util.concurrent.locks.LockSupport;
 @Slf4j
 public class ZkClient {
     public static void main(String[] args) throws Exception {
-
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        ZooKeeper zk = new ZooKeeper("10.1.7.12:2181", 5000, new Watcher() {
-            @Override
-            public void process(WatchedEvent event) {
-                log.info("####事件####" + event);
-                if (event.getState() == Event.KeeperState.SyncConnected) {
-                    countDownLatch.countDown();
-                }
-            }
-        });
-        countDownLatch.await();
-        ZooKeeper.States state = zk.getState();
-        log.info("状态" + state);
-        byte[] data = zk.getData("/abc", true, null);
-        log.info("结果：" + new String(data));
+        AbcZk abcZk = new AbcZk("10.1.7.12:2181");
+        abcZk.start();
         LockSupport.park();
+    }
+
+    static class AbcZk implements Watcher {
+        private ZooKeeper zk;
+        private String    connectString;
+
+        public AbcZk(String connectString) {
+            this.connectString = connectString;
+        }
+
+        public void start() {
+            try {
+                zk = new ZooKeeper(connectString, 30000, this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void process(WatchedEvent watchedEvent) {
+            log.info("####事件####" + watchedEvent);
+            try {
+                log.info("状态：{} 数据：{}", zk.getState(), new String(zk.getData("/abc", true, null)));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
