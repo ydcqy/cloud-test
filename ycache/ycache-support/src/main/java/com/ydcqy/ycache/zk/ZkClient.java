9bab1b4676c49f13a508c4bb21019f33e3a4d8da
xiaoyu <742108726@qq.com>
2018-08-19 11:15:58 +0800

    #

--- ycache/ycache-support/src/main/java/com/ydcqy/ycache/zk/ZkClient.java
+++ ycache/ycache-support/src/main/java/com/ydcqy/ycache/zk/ZkClient.java
@@ -1,27 +1,35 @@
 package com.ydcqy.ycache.zk;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.locks.LockSupport;
 
 /**
  * @author xiaoyu
  */
+@Slf4j
 public class ZkClient {
     public static void main(String[] args) throws Exception {
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
         ZooKeeper zk = new ZooKeeper("10.1.7.12:2181", 5000, new Watcher() {
             @Override
             public void process(WatchedEvent event) {
-                System.out.println("xxxxxxxxxxxxxxxxxxx"+event);
+                log.info("####事件####" + event);
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    countDownLatch.countDown();
+                }
             }
         });
-        System.out.println(zk.getChildren("/", null));
-        System.out.println(new String(zk.getData("/abc", null, null)));
-        LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(10));
-        System.out.println(new String(zk.getData("/abc", null, null)));
-
+        countDownLatch.await();
+        ZooKeeper.States state = zk.getState();
+        log.info("状态" + state);
+        byte[] data = zk.getData("/abc", true, null);
+        log.info("结果：" + new String(data));
+        LockSupport.park();
     }
 }
---
+++ ymq/ymq-support/src/test/java/com/ydcqy/ymq/LogTest.java
@@ -0,0 +1,44 @@
+package com.ydcqy.ymq;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.util.Date;
+
+/**
+ * @author xiaoyu
+ */
+@Slf4j
+public class LogTest {
+    public static void main(String[] args) throws Exception {
+        RateLimiter rateLimiter = RateLimiter.create(100);
+        long ss = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+//            Thread.sleep(1);
+            boolean acquire = rateLimiter.tryAcquire();
+            if (acquire) {
+                JSONObject obj = new JSONObject();
+                obj.put("logType", "ydy");
+                obj.put("userId", RandomUtils.nextInt(100000, 101000));
+                obj.put("createTime", DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss.SSS"));
+                obj.put("success", true);
+                obj.put("body", "用户查询接口调用");
+                log.info(JSON.toJSONString(obj));
+            } else {
+                JSONObject obj = new JSONObject();
+                obj.put("logType", "ydy");
+                obj.put("userId", RandomUtils.nextInt(100000, 101000));
+                obj.put("createTime", DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss.SSS"));
+                obj.put("success", false);
+                obj.put("body", "超频，请稍后再试");
+                log.info(JSON.toJSONString(obj));
+            }
+        }
+
+        System.out.println(System.currentTimeMillis() - ss);
+    }
+}
